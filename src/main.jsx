c5976fa51568f1b6f3a5adfe4b259aea4c7c95af
sariva19860326-sudo <sariva19860326@gmail.com>
2026-05-09 21:55:08 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -178,7 +178,12 @@ function QueueCard({ qKey, state, myNumber, onTake, onRetake }) {
               <div style={{ fontSize:48, fontWeight:900, lineHeight:1, color: isMyTurn || isNext ? "#fff" : "#c07a3a" }}>
                 {String(mine).padStart(3,"0")}
               </div>
-              {!isMyTurn && <WaitEstimate waiting={isNext ? 1 : waiting} mins={q.mins} />}
+              {!isMyTurn && (
+  <WaitEstimate 
+    waiting={isNext ? 1 : waiting} 
+    mins={(isNext ? 1 : waiting) * 10} 
+  />
+)}
             </div>
             <div style={{ textAlign:"right" }}>
               <div style={{ fontSize:11, color: subColor, marginBottom:2 }}>目前叫號</div>
